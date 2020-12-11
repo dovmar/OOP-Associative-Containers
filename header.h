cf7f9338f79c488faf8aa0e2622d9562a4ae48f0
dovmar <71687301+dovmar@users.noreply.github.com>
2020-12-11 12:28:45 +0200

    Initial commit

---
+++ header.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <set>
+#include <map>
+#include <vector>
+#include <regex>
+#include <sstream>
+#include <iomanip>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::istringstream;
+using std::set;
+using std::map;
+using std::vector;
+using std::setw;
+using std::left;
+using std::cout;
---
+++ main.cpp
@@ -0,0 +1,125 @@
+#include "header.h";
+
+void rastiURL(set<string>& urls, string eilute) {
+    // Randa nuorodas pateiktoje eiluteje
+    string zodis;
+    istringstream iss(eilute);
+    while (iss >> zodis) {
+        if (std::regex_match(zodis, std::regex("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")))
+        {
+            urls.insert(zodis);
+        }
+    }
+}
+
+
+string supaprastintiEilute(set<string> urls,string eilute) {
+    // Is eilutes istrina skaicius ir kitus ne raidinius simbolius
+    for (auto url : urls) {
+        eilute = std::regex_replace(eilute, std::regex(url), "");
+    }
+    int ilgis = eilute.size();
+    for (int i = 0; i <= ilgis; i++) {
+        if (ispunct(eilute[i]) || isdigit(eilute[i])) {
+            eilute.erase(i,1);
+            ilgis = eilute.size();
+        }
+        else {
+            eilute[i] = std::tolower(eilute[i]);
+        }
+    }
+    return eilute;
+}
+
+
+
+bool zodisZodyne(map<string, vector<int>>& zodynas, string& zodis) {
+    // Patikrina ar zodyne jau yra tam tikras zodis
+    auto iter = zodynas.begin();
+    while (iter != zodynas.end()) {
+        if (iter->first == zodis) {
+            return true;
+        }
+        iter++;
+    }
+    return false;
+}
+
+
+void papildytiZodyna(map<string, vector<int>>& zodynas, string& zodis, int index) {
+    // Atnaujina informacija apie zodzius kurie jau yra zodyne
+    auto iter = zodynas.find(zodis);
+    iter->second.push_back(index);
+
+}
+
+
+void nuskaitytiTeksta(set<string>& urls, map<string, vector<int>>& zodynas, string failoPav) {
+    // Nuskaito teksta is nurodyto failo
+    string tekstas;
+    string eilute;
+    string zodis;
+    ifstream skait;
+    skait.open(failoPav);
+    for (int i = 0; !skait.eof(); ++i) {
+        getline(skait, eilute);
+        rastiURL(urls,eilute);
+        eilute = supaprastintiEilute(urls,eilute);
+        istringstream iss(eilute);
+        while (iss >> zodis) {
+            if (!zodisZodyne(zodynas,zodis)) {
+                zodynas.insert(std::make_pair(zodis, vector<int>({i+1})));
+            }
+            else {
+                papildytiZodyna(zodynas, zodis, i+1);
+            }
+        }
+    }
+    skait.close();
+}
+
+
+void isvestiPasikartojancius(map<string, vector<int>>& zodynas, string failoPav) {
+    // Isveda pasikartojancius zodzius, kiek kartu pasikartojo ir kokiose eilutese
+    ofstream isved;
+    auto iter = zodynas.begin();
+    isved.open(failoPav);
+    while (iter != zodynas.end()) {
+        isved << setw(20) << left << iter->first << " ";
+        isved << setw(5) << left << iter->second.size() << " ";
+        for (auto eilnr : iter->second) {
+            isved << eilnr << " ";
+        }
+        isved << "\n";
+        iter++;
+    }
+    isved.close();
+}
+
+
+void isvestiURL(set<string>& urls) {
+    // Isveda tekste rastas nuorodas
+    for (string url : urls) {
+        cout << url << "\n";
+    }
+}
+
+void isvestiURL(set<string>& urls, string failoPav) {
+    // Isveda tekste rastas nuorodas i faila
+    ofstream isved;
+    isved.open(failoPav);
+    for (string url : urls) {
+        isved << url << "\n";
+    }
+}
+
+int main()
+{
+    set<string> urls;
+    map<string, vector<int>> zodynas;
+    nuskaitytiTeksta(urls, zodynas, "text.txt");
+    isvestiPasikartojancius(zodynas,"rezultatai.txt");
+    isvestiURL(urls);
+}
+
+    
